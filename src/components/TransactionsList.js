fe65b4eec7878de8dbacc7f07d33cbcfc38ab133
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-11 16:39:14 +0300

    Updated the transaction list with functionality to sorttransactions by either category or description

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Transaction from "./Transaction";
 
 function TransactionsList({ transactions, deleteTransaction }) {
+  const [sortBy, setSortBy] = useState("");
+
+  const handleSort = (type) => {
+    if (sortBy === type) {
+      setSortBy("");
+    } else {
+      setSortBy(type);
+    }
+  };
+
+  const sortedTransactions = [...transactions].sort((a, b) => {
+    if (sortBy === "category") {
+      return a.category.localeCompare(b.category);
+    } else if (sortBy === "description") {
+      return a.description.localeCompare(b.description);
+    }
+    return 0;
+  });
+
   return (
     <table className="ui celled striped padded table">
-      <tbody>
+      <thead>
         <tr>
           <th>
-            <h3 className="ui center aligned header">Date</h3>
+            <button
+              className="ui button"
+              onClick={() => handleSort("date")}
+            >
+              Date
+            </button>
           </th>
           <th>
-            <h3 className="ui center aligned header">Description</h3>
+            <button
+              className="ui button"
+              onClick={() => handleSort("description")}
+            >
+              Description
+            </button>
           </th>
           <th>
-            <h3 className="ui center aligned header">Category</h3>
+            <button
+              className="ui button"
+              onClick={() => handleSort("category")}
+            >
+              Category
+            </button>
           </th>
           <th>
-            <h3 className="ui center aligned header">Amount</h3>
+            <button
+              className="ui button"
+              onClick={() => handleSort("amount")}
+            >
+              Amount
+            </button>
           </th>
           <th>
             <h3 className="ui center aligned header">Action</h3>
           </th>
         </tr>
-        {transactions.map((transaction) => (
+      </thead>
+      <tbody>
+        {sortedTransactions.map((transaction) => (
           <Transaction
             key={transaction.id}
             transaction={transaction}

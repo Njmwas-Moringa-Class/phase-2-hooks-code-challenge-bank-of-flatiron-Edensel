c5517657b51f328c090405f48e5a4c959f2b0def
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-11 16:04:21 +0300

    Updated the Form component to Be able to handle form submission according to the data we have

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({ onAddTransaction }) {
+  const [formData, setFormData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTransaction(formData);
+    setFormData({
+      date: "",
+      description: "",
+      category: "",
+      amount: "",
+    });
+  };
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" value={formData.date} onChange={handleInputChange} />
+          <input type="text" name="description" placeholder="Description" value={formData.description} onChange={handleInputChange} />
+          <input type="text" name="category" placeholder="Category" value={formData.category} onChange={handleInputChange} />
+          <input type="number" name="amount" placeholder="Amount" step="0.01" value={formData.amount} onChange={handleInputChange} />
         </div>
         <button className="ui button" type="submit">
           Add Transaction

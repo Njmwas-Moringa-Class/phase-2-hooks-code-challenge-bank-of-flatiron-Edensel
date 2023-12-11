11f5fb3c7984b7fedbd84c760355eb47e8c2c91f
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-11 16:09:22 +0300

    Updated the account container to allow adding new transactions and updating the local state for display as well as persisting it to the backend

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
+// Function to handle a new post transaction request to the backend API for persistence
+  const handleAddTransaction = (newTransaction) => {
+    fetch("http://localhost:8001/transactions", { 
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTransaction),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTransactions([...transactions, data]); // Add new transaction
+      });
+  };
+
   return (
     <div>
       <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <AddTransactionForm onAddTransaction={handleAddTransaction} />
+      <TransactionsList transactions={transactions} />
     </div>
   );
 }
